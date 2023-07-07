69d9083382c9b91c1d2637406e655cbcbc1a8a1a
James Scarfe <james.scarfe@skybettingandgaming.com>
2023-07-07 14:21:23 +0100

    Added @Serializable tag to the game and player stats

--- src/main/kotlin/com/skybet/gameresultprocessor/repository/GameStats.kt
+++ src/main/kotlin/com/skybet/gameresultprocessor/repository/GameStats.kt
@@ -1,3 +1,6 @@
 package com.skybet.gameresultprocessor.repository
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GameStats(val game: String, val totalStaked: Double, val totalPayout: Double, val totalWins: Int, val totalLosses: Int)
--- src/main/kotlin/com/skybet/gameresultprocessor/repository/PlayerStats.kt
+++ src/main/kotlin/com/skybet/gameresultprocessor/repository/PlayerStats.kt
@@ -1,3 +1,6 @@
 package com.skybet.gameresultprocessor.repository
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class PlayerStats(val user: String, val totalStaked: Double, val totalPayout: Double, val totalWins: Int, val totalLosses: Int, val favouriteGame: String)
